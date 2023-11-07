9c54af4af6f5b51a7fce01386ce9f35b9b465531
Florence C <florencechartrand1027@gmail.com>
2023-11-07 15:55:00 -0500

    AAAAAAAAAAAAAAAAAAAAAAAH

--- scripts/character_countess.gd
+++ scripts/character_countess.gd
@@ -8,6 +8,10 @@ const DASH_VELOCITY = 200
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var nbsauts = 0
+var next_animation = ""
+var current_animation = ""
+
+var hp = 100
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -28,11 +32,17 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and nbsauts == 0:
 		velocity.y = JUMP_VELOCITY
+		next_animation = "jump"
 		nbsauts = 1
 		
 	# Gérer le double saut
-	if Input.is_action_just_pressed("ui_accept") and not is_on_floor() and nbsauts == 1:
+	if Input.is_action_just_pressed("ui_accept") and not is_on_floor() and nbsauts < 2:
 		velocity.y = JUMP_VELOCITY
+		next_animation = "jump"
+		nbsauts = 2
+	
+	# Réinitialiser le nombre de sauts
+	if is_on_floor():
 		nbsauts = 0
 	
 	# Get the input direction and handle the movement/deceleration.
@@ -40,11 +50,33 @@ func _physics_process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		if is_on_floor():
+			next_animation = "run"
+		else:
+			next_animation = "jump"
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if next_animation != current_animation:
+		changer_animation(next_animation)
+		
+	if is_on_floor() and not Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left") and not Input.is_action_just_pressed("ui_accept"):
+		next_animation = "idle"
+		
+		
+	# Gérer la direction de l'animation
+	if (velocity.x >= 0):
+		$AnimatedSpriteCountess.set_flip_h(false)
+	# Sinon
+	elif (velocity.x < 0):
+		$AnimatedSpriteCountess.set_flip_h(true)
 
 	move_and_slide()
 
+"""
+Fonction pour permettre au personnage de dash.
+Consiste à seulement modifier la position du joueur selon sa direction.
+"""
 func dash():
 	# Si le personnage regarde vers la droite
 	if (velocity.x >= 0):
@@ -52,4 +84,12 @@ func dash():
 	# Sinon
 	elif (velocity.x < 0):
 		position.x -= DASH_VELOCITY
-
+		
+"""
+Fonction pour changer l'animation du personnage.
+@param animation : l'animation à utiliser
+"""
+func changer_animation(animation):
+	$AnimatedSpriteCountess.play(animation)
+	current_animation = animation 
+	next_animation = ""
