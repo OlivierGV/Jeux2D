88d3ce843e091645a3423c996c8ced499439b666
AlexisBoivin <112088810+AlexisBoivin@users.noreply.github.com>
2023-11-27 17:03:27 -0500

    forêt fini

--- scripts/niveau_foret.gd
+++ scripts/niveau_foret.gd
@@ -1,6 +1,7 @@
 extends Node
 
-
+var coffreouvert = false
+@onready var animcoffre = $Coffre
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,9 +10,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-func bossMort():
-	$StaticBodyPorteSortie.queue_free()
 
 
 func _on_area_trou_body_entered(body):
@@ -49,3 +47,25 @@ func _on_area_etang_4_body_entered(body):
 func _on_area_etang_4_body_exited(body):
 	body.SPEED = body.SPEED*4
 
+
+func _on_area_etang_5_body_entered(body):
+	body.SPEED = body.SPEED/4
+
+
+func _on_area_etang_5_body_exited(body):
+	body.SPEED = body.SPEED*4
+
+
+func _on_area_etang_6_body_entered(body):
+	body.SPEED = body.SPEED/4
+
+
+func _on_area_etang_6_body_exited(body):
+	body.SPEED = body.SPEED*4
+
+
+func _on_coffre_declencheur_body_entered(body):
+	if body.is_in_group("joueur") and coffreouvert == false:
+		animcoffre.play("ouvert")
+		coffreouvert = true
+		$StaticBodyPorteSortie.queue_free()
